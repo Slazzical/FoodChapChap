8d757bd2c2b30317e89c0ed08ad5c7cb9551ce64
Manya-George <131265976+Manya-George@users.noreply.github.com>
2024-11-24 20:52:07 +0300

    Add files via upload

---
+++ src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.greetingcard
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set up the click listener for the search icon
+        val searchIcon: ImageView = findViewById(R.id.search_cafeteria)
+        searchIcon.setOnClickListener {
+            try {
+                // Log to confirm the click event
+                Log.d("MainActivity", "Search icon clicked")
+
+                val intent = Intent(this, SearchCafeteriaActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                // Log any exceptions to catch potential errors
+                Log.e("MainActivity", "Error launching SearchCafeteriaActivity", e)
+            }
+        }
+        // Set up the click listener for the search icon
+        fun viewCafeteria(view: View) {
+            // Create an intent to navigate to CafeteriaActivity
+            try {
+                // Log to confirm the click event
+                Log.d("MainActivity", "View Button clicked")
+
+                val intent = Intent(this, CafeteriaActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                // Log any exceptions to catch potential errors
+                Log.e("MainActivity", "Error launching CafeteriaActivity", e)
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GreetingCardTheme {
+        MainActivity()
+    }
+}
